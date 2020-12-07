b3ebf4647f07de6efe254ee5d35cd95c960a6b76
sam88khan <samkhanit@gmail.com>
2020-12-07 13:04:03 +0530

    First commit of rest

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	public static WebDriver driver;
+	@BeforeSuite
+	public void setUp() {
+		driver = new ChromeDriver();
+	}
+	@Test
+	public void doLogin() 
+	{
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		driver.findElement(By.xpath("//*[@id=\"login1\"]")).sendKeys("samit");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("122456");
+	}
+	@AfterSuite
+	public void tearDown()
+	{
+		driver.quit();
+	}
+}
